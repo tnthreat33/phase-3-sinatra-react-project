1f95975d066dd99ef5736b8c572af4f062eecb52
Rachel Hamby <tnthreat33@gmail.com>
2023-04-26 22:01:30 -0400

    re-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,33 @@
 puts "🌱 Seeding data..."
 
 # Create teams
-team_names = ["Lions", "Tigers", "Bears", "Cougars", "Panthers"]
-cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]
-states = ["NY", "CA", "IL", "TX", "AZ"]
-teams = []
-
-team_names.first(5).each_with_index do |name, index|
-  team = Team.create(name: name, city: cities[index], state: states[index])
-  teams << team
-end
+center_grove = Team.create(name: "Center Grove", city: "Greenwood", state: "IN")
+avon = Team.create(name: "Avon", city: "Avon", state: "IN")
+new_palestine = Team.create(name: "New Palestine", city: "Palestine", state: "IN")
+columbus_north = Team.create(name: "Columbus North", city: "Columbus", state: "IN")
 
 # Create games
-opponent_names = ["Titans", "Bulldogs", "Eagles", "Warriors", "Trojans"]
-locations = ["New York, NY", "Los Angeles, CA", "Chicago, IL", "Houston, TX", "Phoenix, AZ"]
+Game.create(team: center_grove, opponent: "Whiteland", date: Date.new(2023, 03, 28), location: "Greenwood", team_score: 9, opponent_score: 2)
+Game.create(team: center_grove, opponent: "Zionsville", date: Date.new(2023, 03, 29), location: "Zionsville", team_score: 14, opponent_score: 3)
+Game.create(team: center_grove, opponent: "Pendleton Heights", date: Date.new(2023, 03, 31), location: "Pendleton", team_score: 15, opponent_score: 6)
+Game.create(team: center_grove, opponent: "DCHS", date: Date.new(2023, 04, 8), location: "Pendleton", team_score: 10, opponent_score: 0)
+Game.create(team: center_grove, opponent: "Chesterton", date: Date.new(2023, 04, 8), location: "Pendleton", team_score: 13, opponent_score: 0)
 
-teams.each do |team|
-  opponent_names.shuffle.each_with_index do |opponent_name, index|
-    if team.name != opponent_name
-      opponent_score = rand(50..150)
-      team_score = rand(50..150)
-      location = locations[index % locations.length]
-      date = Time.now + (index * 7).days
-      
-      Game.create(team: team_id, opponent: opponent_name, date: date, location: location,
-                  team_score: team_score, opponent_score: opponent_score)
-    end
-  end
-end
+Game.create(team: avon, opponent: "Lake Central", date: Date.new(2023, 03, 28), location: "Avon", team_score: 7, opponent_score: 5)
+Game.create(team: avon, opponent: "Noblesville", date: Date.new(2023, 03, 28), location: "Avon", team_score: 11, opponent_score: 10)
+Game.create(team: avon, opponent: "Silver Creek", date: Date.new(2023, 04, 01), location: "Indianapolis", team_score: 8, opponent_score: 2)
+Game.create(team: avon, opponent: "BNLHS", date: Date.new(2023, 04, 01), location: "Bedford", team_score: 12, opponent_score: 2)
+Game.create(team: avon, opponent: "Ben Davis", date: Date.new(2023, 04, 04), location: "Avon", team_score: 6, opponent_score: 3)
+
+Game.create(team: new_palestine, opponent: "EHHS", date: Date.new(2023, 03, 20), location: "Palestine", team_score: 5, opponent_score: 1)
+Game.create(team: new_palestine, opponent: "Avon", date: Date.new(2023, 04, 06), location: "Palestine", team_score: 6, opponent_score: 3)
+Game.create(team: new_palestine, opponent: "Plainfield", date: Date.new(2023, 04, 07), location: "Plainfield", team_score: 10, opponent_score: 0)
+Game.create(team: new_palestine, opponent: "East Central", date: Date.new(2023, 03, 20), location: "Fishers", team_score: 7, opponent_score: 11)
+
+Game.create(team: columbus_north, opponent: "Bloomington South", date: Date.new(2023, 3, 28), location: "Columbus", team_score: 5, opponent_score: 2)
+Game.create(team: columbus_north, opponent: "Greenwood", date: Date.new(2023, 3, 30), location: "Greenwood", team_score: 8, opponent_score: 1)
+Game.create(team: columbus_north, opponent: "East Central", date: Date.new(2023, 4, 2), location: "Columbus", team_score: 4, opponent_score: 3)
+Game.create(team: columbus_north, opponent: "Whiteland", date: Date.new(2023, 4, 5), location: "Whiteland", team_score: 6, opponent_score: 7)
+Game.create(team: columbus_north, opponent: "Floyd Central", date: Date.new(2023, 4, 8), location: "Floyd Central", team_score: 3, opponent_score: 5)
+
 puts "✅ Done seeding!"
