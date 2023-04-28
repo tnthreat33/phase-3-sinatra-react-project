343a354d2139c2dd8cde676d28e1f0972225e563
Rachel Hamby <tnthreat33@gmail.com>
2023-04-27 22:06:10 -0400

    post route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,26 +3,40 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/teams" do
-    teams = Team.limit(5)
+    teams = Team.all
     teams.to_json(include: :games)
   end
   
-  get "/games" do
-    games = Game.all 
-    games.to_json(include: :team)
+  delete '/teams/:id' do
+    team = Team.find(params[:id])
+    team.destroy
+    { message: "Team with id #{params[:id]} deleted" }.to_json
   end
 
-  #post "/games" do 
-    #game = Game.create(
-      #opponent: params[:opponent],
-      #date: params[:date],
-     # location: params[:location],
-      #team_score: params[:team_score],
-     # opponent_score: params[:opponent_score],
-     # team_id: params[:team_id]
-   # )
-   # game.to_json
-  #end
+  put '/games/:id' do
+    game = Game.find(params[:id])
+    game.update(
+      opponent: params[:opponent],
+      date: params[:date],
+      location: params[:location],
+      team_score: params[:team_score],
+      opponent_score: params[:opponent_score],
+      team_id: params[:team_id]
+    )
+    game.to_json
+  end
+  
+  post '/games' do
+    game = Game.create(
+      opponent: params[:opponent],
+      date: params[:date],
+      location: params[:location],
+      team_score: params[:team_score],
+      opponent_score: params[:opponent_score],
+      team_id: params[:team_id]
+    )
+    game.to_json
+  end
   
 
 end
